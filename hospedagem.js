efdb4b7a50ffac8e55c613c1f9c0bc8fb6431c3a
unknown <betim009@gmail.com>
2024-12-17 11:03:06 -0300

    update

--- hospedagem.js
+++ hospedagem.js
@@ -1,27 +1,35 @@
 import { hospedagens } from "./src/data/hospedagens.js";
+import { images } from "./src/data/images.js";
 
-const hospedagem = document.getElementById("hospedagem")
+const divHospedagem = document.getElementById("hospedagem")
+const divImage = document.getElementById("image")
+const divImages = document.getElementById("images")
 
-
-
-
+// Coleta a URL
 const url = new URL(window.location.href);
-console.log(url)
 // Cria um objeto URLSearchParams para acessar os parâmetros
 const params = new URLSearchParams(url.search);
-
 // Obtém o valor do parâmetro 'nome'
 const nome = params.get('nome');
+// Econtra a hospedagem pelo parametro da URL
 const findHospedagem = hospedagens.find(e => e.name_hos === nome)
-console.log(findHospedagem)
 
-hospedagem.innerHTML += `
-    <div class='card'>
-    <img class="thumb" src="${findHospedagem.thumb_hos}" alt=""> 
-    <h2>${findHospedagem.name_hos}</h2>
-    <p>${findHospedagem.cidade_hos}, ${findHospedagem.uf_hos}</p>
-    <p>Avaliação: ${findHospedagem.nota_hos}</p>
-    <a href="hospedagem.html?nome=${findHospedagem.name_hos}">Clique aqui</a>
-    </div>   `
+divImage.innerHTML += `
+    <div>
+        <img class="img1" src="${findHospedagem.thumb_hos}" alt=""> 
+    </div>`
 
+images.forEach(element => {
+    divImages.innerHTML += `
+        <img class="img2" src="${element}" alt=""> 
+    `
+});
+
+divHospedagem.innerHTML += `
+    <div>
+        <h2>${findHospedagem.name_hos}</h2>
+
+
+    </div>
+`
 
---
+++ src/data/images.js
@@ -0,0 +1,6 @@
+export const images = [
+    'src/assets/1.png',
+    'src/assets/2.png',
+    'src/assets/1.png',
+    'src/assets/2.png'
+]
